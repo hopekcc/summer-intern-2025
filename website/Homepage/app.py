5359688c6ebc07643fd0958fcbb5b63a85f70184
ryan-hsu-17 <ryanhsu@berkeley.edu>
2025-07-14 11:20:10 -0700

    WIP: in progress changes

--- website/Homepage/app.py
+++ website/Homepage/app.py
@@ -1,8 +1,15 @@
 
 from flask import Flask, render_template
 
-app = Flask(__name__)
+import firebase_admin
+from firebase_admin import credentials, auth
 
+app = Flask(__name__)
+'''
+# Initialize Firebase
+cred = credentials.Certificate("firebase_key.json")  # or the exact name of your downloaded key
+firebase_admin.initialize_app(cred)
+'''
 @app.route('/')
 def home():
     return render_template('home.html', username='Ryan', insert_text='Wish you have a good day!!!')
