a43e4100336025d790f0b10c75da4da818f7f84e
Jonathan Yu <2jonathanjty@gmail.com>
2025-08-04 21:51:16 -0700

    login screen

--- mobile/chordproapp/app/src/main/java/com/example/chordproapp/MainActivity.kt
+++ mobile/chordproapp/app/src/main/java/com/example/chordproapp/MainActivity.kt
@@ -36,4 +36,5 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+
 }
--- mobile/chordproapp/app/src/main/java/com/example/chordproapp/screens/SearchScreen.kt
+++ mobile/chordproapp/app/src/main/java/com/example/chordproapp/screens/SearchScreen.kt
@@ -1,20 +1,57 @@
 package com.example.chordproapp.screens
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.foundation.shape.RoundedCornerShape
+
 
 @Composable
 fun SearchScreen() {
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(30.dp)
-    ) {
+
+    var query by remember { mutableStateOf("") }
+
+    Column(modifier = Modifier.fillMaxSize().padding(30.dp)) {
         Text("Search", fontSize = 30.sp, fontWeight = FontWeight.Bold)
+        Spacer(modifier = Modifier.height(20.dp))
+        SearchBar(
+            query = query,
+            onQueryChange = { query = it }
+        )
     }
+}
+
+@Composable
+fun SearchBar(
+    query: String,
+    onQueryChange: (String) -> Unit,
+    modifier: Modifier = Modifier,
+    placeholder: String = "Search",
+) {
+    TextField(
+        value = query,
+        onValueChange = onQueryChange,
+        leadingIcon = {
+            Icon(imageVector = Icons.Default.Search, contentDescription = "Search Icon")
+        },
+        placeholder = { Text(placeholder) },
+        singleLine = true,
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+            .background(Color(0xFFF0F0F0), shape = RoundedCornerShape(8.dp)),
+        colors = TextFieldDefaults.colors(
+            focusedContainerColor = Color.Transparent,
+            unfocusedContainerColor = Color.Transparent,
+            disabledContainerColor = Color.Transparent
+        )
+    )
 }
